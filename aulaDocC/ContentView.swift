1f6937e90a3e4d7d15717d833e0f8fe3947f3ff0
Thiago Leandro Liporace <85422792+thiliporace@users.noreply.github.com>
2023-11-09 16:37:21 -0300

    Initial Commit

---
+++ aulaDocC/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  aulaDocC
+//
+//  Created by Thiago Liporace on 09/11/23.
+//
+
+import SwiftUI
+
+///``content``
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ aulaDocC/aulaDocCApp.swift
@@ -0,0 +1,17 @@
+//
+//  aulaDocCApp.swift
+//  aulaDocC
+//
+//  Created by Thiago Liporace on 09/11/23.
+//
+
+import SwiftUI
+
+@main
+struct aulaDocCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
